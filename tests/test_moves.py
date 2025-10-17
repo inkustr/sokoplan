fd6a489bdfbee9e2eb053a72c6700f3c3db69347
nikita.filenko <nikitafil2003@gmail.com>
2025-10-17 13:33:24 +0200

    Fix test

--- tests/test_moves.py
+++ tests/test_moves.py
@@ -12,10 +12,13 @@ LVL = """
 def test_successors_single_push():
     s = parse_level_str(LVL)
     succs = successors_pushes(s)
-    assert len(succs) == 1
-    ns = succs[0]
-    # the box should be on the goal
-    assert ns.is_goal()
+    assert len(succs) == 4
+    
+    goal_states = [succ for succ in succs if succ.is_goal()]
+    assert len(goal_states) == 1
+    
+    goal_state = goal_states[0]
+    assert goal_state.is_goal()
 
 
 def test_reachability_non_empty():
